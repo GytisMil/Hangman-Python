accd513dbc68e88f37fea2b77a8b5e9a6ba98ae4
GytisMil <gytis.miliauskas963@gmail.com>
2023-08-07 11:43:05 +0300

    Made basic interface to select SP or MP

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,16 @@
 #Main file to launch Hangman game
+import Singleplayer_functions
+
+print("Welcome to Hangman game made in Python.")
+print("Play Singleplayer (S) or Multiplayer (M)?")
+print("If you launched this file accidentally, then you can type 'Q' to quit.")
+mode = input()
+while(mode != "S" and mode != "s" and mode != "M" and mode != "m" and mode != "Q" and mode != "q"):
+    print("Invalid input.")
+    print("Play Singleplayer (S) or Multiplayer (M)?")
+    print("If you launched this file accidentally, then you can type 'Q' to quit.")
+    mode = input()
+if(mode == "S" or mode == "s"):
+    print("Singleplayer mode selected!")
+elif(mode == "M" or mode == "m"):
+    print("Multiplayer mode selected!")
